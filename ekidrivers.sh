55ba557abc05bac47c7512cd6a6890d9b3d925b5
michel memeteau <mm@ekimia.fr>
2019-12-12 14:41:20 +0100

    Update ekidrivers.sh

--- ekidrivers.sh
+++ ekidrivers.sh
@@ -12,7 +12,7 @@ unzip -o ekimiadrivers-master.zip
 cd $dir_ekimia 
 
 echo "instaling old python"
-sudo apt install python2.7
+sudo apt install python2.7 -y
 
 echo "# Start Update for Ubuntu 18.04 , please wait up to 20 minutes "
 sudo python2.7 ubuntu-14.04-postinstall.py -c ubuntu-18.04-unity-postinstall.cfg
