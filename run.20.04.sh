d32f17f8de58b476dbcc374364b3e55f0c3383cf
michel memeteau <mm@ekimia.fr>
2020-10-09 11:50:32 +0200

    Update run.20.04.sh

--- run.20.04.sh
+++ run.20.04.sh
@@ -2,6 +2,6 @@
 echo "instaling old python"
 sudo apt install python2.7 -y
 
-echo "# Start Update for Ubuntu 18.04 , please wait up to 20 minutes "
+echo "# Start Install for Ubuntu 20.04 "
 python2.7 ubuntu-14.04-postinstall.py -c ubuntu-20.04-unity-postinstall.cfg
 echo "Finished"
