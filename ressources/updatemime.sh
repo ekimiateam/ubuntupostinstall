6978a9b54639fd746e74bd89a1e4e4a6f93029a0
Evann1609 <133024225+Evann1609@users.noreply.github.com>
2023-05-10 15:54:37 +0200

    script to update mime types for local user

---
+++ ressources/updatemime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+xdg-mime default org.gnome.Nautilus.desktop application/x-ar
+xdg-mime default org.gnome.Nautilus.desktop application/x-arj
+xdg-mime default org.gnome.Nautilus.desktop application/x-compressed-tar
+xdg-mime default org.gnome.Nautilus.desktop application/x-compress
+xdg-mime default org.gnome.Nautilus.desktop application/x-ear
+xdg-mime default org.gnome.Nautilus.desktop application/x-gzip
+xdg-mime default org.gnome.Nautilus.desktop application/x-lha
+xdg-mime default org.gnome.Nautilus.desktop application/x-lhz
+xdg-mime default org.gnome.Nautilus.desktop application/x-lzop
+xdg-mime default org.gnome.Nautilus.desktop application/x-rar-compressed
+xdg-mime default org.gnome.Nautilus.desktop application/x-rar
+xdg-mime default org.gnome.Nautilus.desktop application/x-rpm
+xdg-mime default org.gnome.Nautilus.desktop application/x-tar
+xdg-mime default org.gnome.Nautilus.desktop application/x-war
+xdg-mime default org.gnome.Nautilus.desktop application/x-zip-compressed
+xdg-mime default org.gnome.Nautilus.desktop application/x-zip
+xdg-mime default org.gnome.Nautilus.desktop application/x-zoo
+xdg-mime default org.gnome.Nautilus.desktop application/zip
+xdg-mime default org.gnome.Nautilus.desktop multipart/x-zip
+xdg-mime default org.gnome.Nautilus.desktop application/x-xz-compressed-tar
