b9397fd35fcb359da0965b2c0670e5237614c7a2
Evann1609 <133024225+Evann1609@users.noreply.github.com>
2023-06-02 14:42:50 +0200

    Update ekidrivers.sh

--- ekidrivers.sh
+++ ekidrivers.sh
@@ -2,25 +2,8 @@
 
 echo"Starting Ekimia drivers Update"
 dir_ekimia="ubuntupostinstall-master"
-
 echo "# Getting git archive"
-wget https://github.com/EkimiaSAS/ubuntupostinstall/archive/master.zip -O ekimiadrivers-master.zip 
-
-echo "# Unzip archive"
-unzip -o ekimiadrivers-master.zip
-
 wget https://github.com/EkimiaSAS/ubuntupostinstall/archive/master.zip -O ekimiadrivers-master.zip 
 unzip -o ekimiadrivers-master.zip 
 cd $dir_ekimia 
-
-echo "instaling old python"
-sudo apt install python2.7 -y
-
-echo "# Start Update for Ubuntu 18.04 , please wait up to 20 minutes "
-sudo python2.7 ubuntu-14.04-postinstall.py -c ubuntu-18.04-unity-postinstall.cfg
-
-echo "# Cleaning up"
-rm -rf $dir_ekimia
-
-echo "# Update finished, please reboot "
 bash run.py
