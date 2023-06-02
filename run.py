f6824d38feba8d7eda5b2863f0f2cb56a62907ec
Evann1609 <133024225+Evann1609@users.noreply.github.com>
2023-06-02 14:51:36 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,33 +2,38 @@
 import subprocess
 import distro
 
-def install_ubuntu_22():
+config_file = "NoconfigFile"
+
+def install_ubuntu_22(config_file):
     print('Installation pour Ubuntu 22')
-config_file = "ubuntu-22.04-postinstall.cfg"
+    config_file = "ubuntu-22.04-postinstall.cfg"
+    return config_file
+
     
-def install_ubuntu_20():
+def install_ubuntu_20(config_file):
     print('Installation pour Ubuntu 20')
-config_file = "ubuntu-20.04-unity-postinstall.cfg "
+    config_file = "ubuntu-20.04-unity-postinstall.cfg "
+    return config_file
 
-def install_ubuntu_18():
+def install_ubuntu_18(config_file):
     print('Installation pour Ubuntu 18')
-config_file = "ubuntu-18.04-unity-postinstall.cfg"
+    config_file = "ubuntu-18.04-unity-postinstall.cfg"
+    return config_file
 
-def install_kubuntu_22():
+def install_kubuntu_22(config_file):
     print('Installation pour Kubuntu 22')
-config_file = "kubuntu-22.04.cfg"
+    config_file = "kubuntu-22.04.cfg"
+    return config_file
 
-def install_kubuntu_20():
+def install_kubuntu_20(config_file):
     print('Installation pour Kubuntu 20')
-config_file = "kubuntu-20.04.cfg"
+    config_file = "kubuntu-20.04.cfg"
+    return config_file
 
-def install_mint_21():
+def install_mint_21(config_file):
     print('Installation pour Mint 21')
-config_file = "mint21.cfg"
-
-
-subprocess.run(['python2.7', 'ubuntu-14.04-postinstall.py', '-c', config_file])
-
+    config_file = "mint21.cfg"
+    return config_file
 
 # Dictionnaire des options
 options = {
@@ -49,6 +54,8 @@ install_function = options.get((distro_codename, distro_id))
 
 # Exécution de la fonction si elle existe, sinon affichage d'un message d'erreur
 if install_function:
-    install_function()
+    config_file = install_function(config_file)
+    print(" config file is "+config_file)
+    subprocess.run(['python2.7', 'ubuntu-14.04-postinstall.py', '-c', config_file])
 else:
     print('Installation non prise en charge pour cette distribution.')
