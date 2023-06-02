f8cf58da57accda6dd7e03271be636113aa96485
Evann1609 <133024225+Evann1609@users.noreply.github.com>
2023-06-02 12:50:26 +0200

    Update ekidrivers.sh

--- ekidrivers.sh
+++ ekidrivers.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-import distro
 
 echo"Starting Ekimia drivers Update"
 dir_ekimia="ubuntupostinstall-master"
 
 echo "# Getting git archive"
+wget https://github.com/EkimiaSAS/ubuntupostinstall/archive/master.zip -O ekimiadrivers-master.zip 
+
+echo "# Unzip archive"
+unzip -o ekimiadrivers-master.zip
 
 wget https://github.com/EkimiaSAS/ubuntupostinstall/archive/master.zip -O ekimiadrivers-master.zip 
 unzip -o ekimiadrivers-master.zip 
 cd $dir_ekimia 
-if distro.codename()=='jammy' and distro.id()=='ubuntu': 
-  print('Installation pour Ubuntu 22')
-  bash run.22.04.sh 
-elif distro.codename()=='focal' and distro.id()=='ubuntu':
-  print('Installation pour Ubuntu 20')
-  bash run.20.04.sh 
-elif distro.codename()=='bionic':
-  print('Installation pour Ubuntu 18')
-  bash run.18.04.sh
-elif distro.codename()=='jammy' and distro.id()=='kubuntu':
-  print('Installation pour Kubuntu 22')
-  bash run.kde22.04.sh 
-elif distro.codename()=='focal' and distro.id()=='kubuntu':
-  print('Installation pour Kubuntu 20')
-  bash run.kde20.04.sh
-elif distro.codename()=='vanessa':
-  print('Installation pour Mint 21')
-  bash run.mint21.sh
+
+echo "instaling old python"
+sudo apt install python2.7 -y
+
+echo "# Start Update for Ubuntu 18.04 , please wait up to 20 minutes "
+sudo python2.7 ubuntu-14.04-postinstall.py -c ubuntu-18.04-unity-postinstall.cfg
+
+echo "# Cleaning up"
+rm -rf $dir_ekimia
+
+echo "# Update finished, please reboot "
+bash run.py
