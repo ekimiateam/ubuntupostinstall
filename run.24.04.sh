e872c70d505d11dfedca24832304020227b38506
soren <soren.appa@hotmail.com>
2024-09-18 18:20:23 +0200

    No need to confirm

--- run.24.04.sh
+++ run.24.04.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #reload packages first 
-sudo apt update
-sudo apt upgrade
+sudo apt update 
+sudo apt upgrade -y
 sudo ubuntu-drivers autoinstall
 
-sudo apt install linux-generic-hwe-24.04
-sudo apt install gimp gimp-plugin-registry cheese regionset libdvd-pkg gnome-sound-recorder
-sudo apt install nautilus-admin nautilus-image-converter seahorse-nautilus ooo-thumbnailer
-sudo apt install htop preload gparted hardinfo xsensors synaptic gdebi mtp-tools dkms thermald net-tools lm-sensors
-sudo apt install gnome-tweak-tool dconf-editor
-sudo apt install libva-glx2 libva-x11-2 vainfo mesa-va-drivers
-sudo apt install intel-media-va-driver-non-free
-sudo apt install intel-gpu-tools
-sudo apt install vlc vlc-l10n
-sudo apt install libreoffice-base libreoffice-sdbc-hsqldb
-sudo apt install exfatprogs
-sudo apt install cifs-utils samba smbclient
-sudo apt install ppa-purge powertop
-sudo apt install printer-driver-all-enforce
-sudo apt install winbind
-sudo apt install gnome-shell-extension-manager gnome-shell-extensions
-sudo apt install nextcloud-desktop nautilus-nextcloud
-sudo apt install sssd-ad sssd-tools realmd adcli
-sudo apt install gnome-software-plugin-flatpak
-sudo apt remove gnome-software-plugin-snap
+sudo apt install -y linux-generic-hwe-24.04
+sudo apt install -y gimp gimp-plugin-registry cheese regionset libdvd-pkg gnome-sound-recorder
+sudo apt install -y nautilus-admin nautilus-image-converter seahorse-nautilus ooo-thumbnailer
+sudo apt install -y htop preload gparted hardinfo xsensors synaptic gdebi mtp-tools dkms thermald net-tools lm-sensors
+sudo apt install -y gnome-tweak-tool dconf-editor
+sudo apt install -y libva-glx2 libva-x11-2 vainfo mesa-va-drivers
+sudo apt install -y intel-media-va-driver-non-free
+sudo apt install -y intel-gpu-tools
+sudo apt install -y vlc vlc-l10n
+sudo apt install -y libreoffice-base libreoffice-sdbc-hsqldb
+sudo apt install -y exfatprogs
+sudo apt install -y cifs-utils samba smbclient
+sudo apt install -y ppa-purge powertop
+sudo apt install -y printer-driver-all-enforce
+sudo apt install -y winbind
+sudo apt install -y gnome-shell-extension-manager gnome-shell-extensions
+sudo apt install -y nextcloud-desktop nautilus-nextcloud
+sudo apt install -y sssd-ad sssd-tools realmd adcli
+sudo apt install -y gnome-software-plugin-flatpak
+sudo apt remove -y gnome-software-plugin-snap
 
 sudo dpkg-reconfigure --frontend=noninteractive libdvd-pkg
 
